ea2e1852de8e9274bfeff46c50adf0575dacb7e0
xqgu <281557868@qq.com>
2018-06-04 14:18:36 +0800

    20180530-3

--- hrp-test/test-impl/src/test/java/hrp/test/tools/api/service/element/window/WindowFormElementService.java
+++ hrp-test/test-impl/src/test/java/hrp/test/tools/api/service/element/window/WindowFormElementService.java
@@ -19,8 +19,8 @@ public interface WindowFormElementService {
 	 * @throws Exception
 	 *             使用Tread
 	 */
-	public void rowFieldWrite(WebDriver driver, int windowLevel, String windowName, String rowNumber,
-			String targetFieldName, String writeSometing) throws Exception;
+	public void fieldWrite(WebDriver driver, int windowLevel, String windowName, String rowNumber,
+						   String targetFieldName, String writeSometing) throws Exception;
 
 	/**
 	 * 在弹窗内的单元表格下的指定行数的单元框的下拉列表内选择对应的值 （一般使用在报账系统内）
@@ -38,8 +38,8 @@ public interface WindowFormElementService {
 	 * @throws Exception
 	 *             使用Tread
 	 */
-	public void rowFieldDownListSelect(WebDriver driver, int windowLevel, String windowName, String rowNumber,
-			String targetFieldName, String writeSometing) throws Exception;
+	public void fieldDownListSelect(WebDriver driver, int windowLevel, String windowName, String rowNumber,
+									String targetFieldName, String writeSometing) throws Exception;
 
 	/**
 	 * 在弹窗内的单元表格下的指定行数的单元框内填写相应的数据并查找对应的值 （一般使用在报账系统内）
@@ -57,8 +57,8 @@ public interface WindowFormElementService {
 	 * @throws Exception
 	 *             使用Tread
 	 */
-	public void rowFieldDownListWriteSearch(WebDriver driver, int windowLevel, String windowName, String rowNumber,
-			String targetFieldName, String writeSometing) throws Exception;
+	public void fieldDownListWriteSearch(WebDriver driver, int windowLevel, String windowName, String rowNumber,
+										 String targetFieldName, String writeSometing) throws Exception;
 
 	/**
 	 * 指定窗口下含有父目录的子项目下填写值
@@ -80,4 +80,19 @@ public interface WindowFormElementService {
 	 */
 	public void listFieldCheckButtonWrite(WebDriver driver, int windowLevel, String windowName, String SearchFatherName,
 			String SearchName, String budgetAmount) throws Exception;
+
+	/**
+	 * 在一个二位表单中，使用多个查询值，来找到指定的填写位置进行填写
+	 *
+	 * @param driver                 固定参数
+	 * @param windowLevel            窗口级次
+	 * @param windowName             弹窗名称
+	 * @param targetFieldName        目标填写名称
+	 * @param writeSometing          目标填写值
+	 * @param originTargetFieldCodes 一组或多组查询的参数值
+	 * @throws Exception
+	 */
+	public void guideFieldWrite(WebDriver driver, int windowLevel, String windowName,
+								String targetFieldName, String writeSometing,
+								String... originTargetFieldCodes) throws Exception;
 }
