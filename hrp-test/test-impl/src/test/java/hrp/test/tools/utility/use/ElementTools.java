c462e9a65cc9edf127f4653d49987a1fdd19e749
xqgu <281557868@qq.com>
2018-06-04 14:18:07 +0800

    20180530-3

---
+++ hrp-test/test-impl/src/test/java/hrp/test/tools/utility/use/ElementTools.java
@@ -0,0 +1,23 @@
+package hrp.test.tools.utility.use;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ElementTools {
+    /**
+     * 处理二维度表单的CLASS并从CLASS内取ITEM的名称
+     *
+     * @param driver   固定参数
+     * @param selector 查找CLASS的方法及路径
+     * @return 返回ITEM名称
+     */
+    public static String getFormListClassItemName(WebDriver driver, By selector) {
+        String getClassName = driver.findElement(selector).getAttribute("class")
+                .replaceAll("x-grid3-header", "")
+                .replaceAll("x-grid3-hd", "")
+                .replaceAll("x-grid3-cell", "")
+                .replaceAll("x-grid3-td-", "")
+                .replaceAll("\\s", "");
+        return getClassName;
+    }
+}
